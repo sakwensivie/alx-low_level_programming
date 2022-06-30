8287fa37860232a17426c4258df4419648b62b3f
Samuel Akwensivie <sakwensivie@gmail.com>
2022-06-30 15:57:13 +0000

    All mandatory tasks done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ *  main - Entry point
+ *
+ *  A c program that prints a string
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
