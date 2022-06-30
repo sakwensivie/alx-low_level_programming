7123e80310161a5b1ea42047e909665efbe31037
Samuel Akwensivie <sakwensivie@gmail.com>
2022-06-30 20:44:53 +0000

    The last commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
- * main-Entry point
+ * main - Entry point
  *
  * A program to print
  *
- * Return : Always 0 (success)
+ * Return: Always 0 (success)
  *
  */
 int main(void)
