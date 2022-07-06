b2e22e2bb3b0da47955dbfb99749837356208810
Samuel Akwensivie <sakwensivie@gmail.com>
2022-07-06 17:42:38 +0000

    The third commit today

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * largest_number - Prints the largest number out of 3 numbers.
+ *
+ * @a: first number
+ * @b: second number
+ * @c: third number
+ * Return: the largest among the 3 values
+ */
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if ((a > b) && (a > c))
+		largest = a;
+	else if ((b > a) && (b > c))
+		largest = b;
+	else if ((c > a) && (c > b))
+		largest = c;
+
+	return (largest);
+}
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,6 @@
 #include<stdio>
 
 void positive_or_negative(int i);
-
+int largest_number(int a, int b, int c);
 
 #endif
