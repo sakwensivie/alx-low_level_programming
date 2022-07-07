34de54d56d0c756dd7ac31af6da50d10184c06e4
Samuel Akwensivie <sakwensivie@gmail.com>
2022-07-07 22:13:15 +0000

    My last commit of the day

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+/**
+ * main - prints the prime factors of a number
+ *
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	long int n;
+	int p;
+
+	n = 612852475143;
+	p = 2;
+
+	do {
+		if (n % p == 0)
+		{
+			printf("%d, ", p);
+			n = n / p;
+		}
+		else
+		{
+			p++;
+		}
+	} while (n > p * p);
+	printf("%ld\n", n);
+
+	return (0);
+}
