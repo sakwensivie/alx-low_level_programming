c8b3e78f61ab31c8979cad17bbe6dd1c7e8921e2
Samuel Akwensivie <sakwensivie@gmail.com>
2022-07-07 16:22:45 +0000

    The actuall real last mandatory commit

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,14 +7,18 @@
 void print_square(int size)
 {
 	int i, j;
-
-	for (i = 1; i <= size; i++)
-	{
-		for (j = 0; j < size; j++)
-		{
-			_putchar('#');
-		}
+	if (size <= 0)
 		_putchar('\n');
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
 }
 
