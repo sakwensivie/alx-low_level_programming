7caa06eee4859a7dab11caac7b986a12873778b1
Samuel Akwensivie <sakwensivie@gmail.com>
2022-07-07 19:32:46 +0000

    The truly last debugging commit

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,9 +11,8 @@
  */
 void print_remaining_days(int month, int day, int year)
 {
-	int y;
 
-	if ((y % 4 == 0) || ((y % 100 == 0) && (y % 400 == 0)))
+	if ((year % 4 == 0) || ((year % 100 == 0) && (year % 400 == 0)))
 	{
 		if (month >= 2 && day >= 60)
 			day++;
