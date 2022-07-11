f0365639c9993727319ac565aff9852da207c624
Samuel Akwensivie <sakwensivie@gmail.com>
2022-07-11 23:13:25 +0000

    Another one

---
+++ 0x05-pointers_arrays_strings/5-main.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+ * main - entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	char s[10] = "My School";
+
+	printf("%s\n", s);
+	rev_string(s);
+	printf("%s\n", s);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include"main.h"
+/**
+ * rev_string - reverses the string
+ * @s: input as character array
+ * Return: none
+ */
+void rev_string(char *s)
+{
+	int i, j;
+	char *c, tmp;
+
+	c = s
+	i = 0;
+
+	while (1)
+	{
+		if (s[i] != '\0')
+			i++;
+		else
+			break;
+	}
+	j = i;
+	while (1)
+	{
+		if (j != 0)
+		{
+			c[j] = tmp;
+			c
+		}
+		else
+		{
+
+}
