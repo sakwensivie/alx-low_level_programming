0ba7306a722ae5967f5ae9ff4f1bb485626d76a6
Samuel Akwensivie <sakwensivie@gmail.com>
2022-07-14 11:55:19 +0000

    To upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ * *string_toupper - converts lowercase to uppercase
+ * @str: input as character array
+ * Return: character array
+ */
+char *string_toupper(char *str)
+{
+	int i, j;
+
+	char *upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *lower = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; lower[j] != '\0'; j++)
+		{
+			if (str[i] == lower[j])
+			{
+				str[i] = upper[j];
+			}
+		}
+	}
+	
+	return (str);
+}
