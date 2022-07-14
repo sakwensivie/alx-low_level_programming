4f18ba207ba9fd85120f556aad60182b7ea9667f
Samuel Akwensivie <sakwensivie@gmail.com>
2022-07-14 10:26:28 +0000

    string compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,19 +7,22 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i, flag;
 
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0')
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
 		if (s1[i] > s2[i])
 		{
-			return (1);
+			flag = 1;
+			break;
 		}
 		else if (s1[i] < s2[i])
 		{
-			return (-1);
+			flag = -1;
+			break;
 		}
 		else
-			return (0);
+			flag = 0;
 	}
+	return (flag);
 }
