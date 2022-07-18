826c53dc2819624c44675fb0e4ae45789fbc7383
Samuel Akwensivie <sakwensivie@gmail.com>
2022-07-18 19:37:00 +0000

    correction on memset

---
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+
+
+
+
+int main(void)
+{
+	char a[6] = "hello";
+
+	char b = 'u';
+
+	unsigned int n = 4;
+
+	_memset(a, b, n);
+
+	return (0);
+}
--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,7 +7,7 @@
  * @n: input as an unsigned integer
  * Return: a a pointer to an array
  */
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
