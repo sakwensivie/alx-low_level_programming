f8ced38eccc5b62922602ee78aba389323bf1d9a
Samuel Akwensivie <sakwensivie@gmail.com>
2022-07-20 22:14:40 +0000

    My first commit of the day

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * _puts_recursion - prints a string
+ * @s: input as a string
+ * Return: none
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+		_puts_recursion(s + 1);
+		_putchar(*s);
+	}
+}
