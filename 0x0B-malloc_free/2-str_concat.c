c9aa56e7fda37bac929940aef20581c4212191af
Samuel Akwensivie <sakwensivie@gmail.com>
2022-07-26 20:28:39 +0000

    str concactenation

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,33 @@
+#include"main.h"
+/**
+ * str_concat - concatenate string
+ * @s1:  input as a string
+ * @s2: input as a string
+ * Return: a pointer to the concatenated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int i, j, k;
+	char *ptr;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (i = 0; *(s1 + i) != '\0'; i++)
+		;
+	for (j = 0; *(s2 + j) != '\0'; j++)
+		;
+	ptr = (char *)malloc(sizeof(char) * (i + j + 1));
+	if (ptr == NULL)
+		return (NULL);
+	for (k = 0; k < i; k++)
+	{
+		*(ptr + i) = *(s1 + i);
+	}
+	for (k = 0; k <= j; k++)
+	{
+		*(ptr + i + j) = *(s2 + j);
+	}
+	return (ptr);
+}
