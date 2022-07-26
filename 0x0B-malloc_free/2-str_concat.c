54b8f09196eeaeb915dd720df95ee5918e92a932
Samuel Akwensivie <sakwensivie@gmail.com>
2022-07-26 20:39:12 +0000

    str concactenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,9 +11,9 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 
 	if (s1 == NULL)
-		*s1 = "";
+		s1 = "";
 	if (s2 == NULL)
-		*s2 = "";
+		s2 = "";
 	for (i = 0; *(s1 + i) != '\0'; i++)
 		;
 	for (j = 0; *(s2 + j) != '\0'; j++)
@@ -27,7 +27,7 @@ char *str_concat(char *s1, char *s2)
 	}
 	for (k = 0; k <= j; k++)
 	{
-		*(ptr + i + j + 1) = *(s2 + j);
+		*(ptr + i + k + 1) = *(s2 + j);
 	}
 	return (ptr);
 }
