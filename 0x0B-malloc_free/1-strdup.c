71c06a0be0d06b5b741577db475952f7f28741d8
Samuel Akwensivie <sakwensivie@gmail.com>
2022-07-26 19:54:10 +0000

    strdup correction

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,7 +11,7 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-	for (i = 0; i != '\0'; i++)
+	for (i = 0; *(str + i) != '\0'; i++)
 		;
 	ptr = (char *)malloc(sizeof(char) * (i + 1));
 
