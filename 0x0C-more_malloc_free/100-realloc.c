7dae279e682d59553fbe59f0cb957f816a2bcf81
Samuel Akwensivie <sakwensivie@gmail.com>
2022-07-28 22:48:49 +0000

    memory reallocation

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,28 @@
+#include"main.h"
+
+/**
+ * _realloc - memory reallocation
+ * @ptr: input as a void pointer
+ * @new_size: input as an unsigned integer
+ * @old_size: input as an unsigned integer
+ * Return: a new pointer with the new allocated memory
+ */
+
+void (void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	if (ptr == NULL)
+		ptr = malloc(new_size);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size == old_size)
+		return (ptr);
+
+	free(ptr);
+
+	ptr = malloc(new_size);
+
+	return (ptr);
+}
