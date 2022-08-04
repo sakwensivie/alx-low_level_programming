994556057b09da94951d051011594c3ed4442118
Samuel Akwensivie <sakwensivie@gmail.com>
2022-08-04 21:58:59 +0000

    Gina is disturbing me

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,4 +1,4 @@
-#include"variadic_functions"
+#include"variadic_functions.h"
 
 /**
  * print_all - prints in all formats
@@ -24,28 +24,28 @@ void print_all(const char * const format, ...)
 				printf(", ");
 				break;
 			} j++;
+		}
 			switch (format[i])
-		{
-		case 'c':
-			printf("%c", va_arg(valist, int)), c = 1;
-			break;
-		case 'i':
-			printf("%d", va_arg(valist, int)), c = 1;
-			break;
-		case 'f':
-			printf("%f", va_arg(valist, double)), c = 1;
-			break;
-		case 's':
-			str = va_arg(valist, char *), c = 1;
-			if (!str)
 			{
-				printf("(nil)");
-				break;
-			}
-			printf("%s", str);
-			break;
-		} i++;
+				case 'c':
+					printf("%c", va_arg(valist, int)), c = 1;
+					break;
+				case 'i':
+					printf("%d", va_arg(valist, int)), c = 1;
+					break;
+				case 'f':
+					printf("%f", va_arg(valist, double)), c = 1;
+					break;
+				case 's':
+					str = va_arg(valist, char *), c = 1;
+					if (!str)
+					{
+						printf("(nil)");
+						break;
+					}
+					printf("%s", str);
+					break;
+			} i++;
 		}
 		printf("\n"), va_end(valist);
-	}
 }
