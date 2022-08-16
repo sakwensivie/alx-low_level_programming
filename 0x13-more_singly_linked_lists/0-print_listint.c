f8fe6a5e1425e876c29b3117d12cdc44c31ec709
Samuel Akwensivie <sakwensivie@gmail.com>
2022-08-16 16:23:29 +0000

    My first commit of today on singly linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include"lists.h"
+
+/**
+ * print_listint - prints all elements of a linked list
+ * @h: a pointer to the head of the linked list
+ * Return: returns the number of elements in the list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+The directory on singly linked lists
